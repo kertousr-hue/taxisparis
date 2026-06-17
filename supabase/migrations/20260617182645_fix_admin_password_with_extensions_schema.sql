f0328283bc8e4d0c86765a33604f2c9d456a275e
kertousr-hue <kertous.r@gmail.com>
2026-06-17 20:50:15 +0200

    Deleted image copy.png

--- supabase/migrations/20260617182645_fix_admin_password_with_extensions_schema.sql
+++
@@ -1,4 +0,0 @@
--- Fix: use extensions.crypt to match the admin_login function
-UPDATE admin_users
-SET password_hash = extensions.crypt('Reda1029', extensions.gen_salt('bf'))
-WHERE email = 'kertous.r@gmail.com';
