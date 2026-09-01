d47a6fecc19aaf126184b260f32d1d29eaa3b706
kertousr-hue <kertous.r@gmail.com>
2026-09-01 04:01:42 +0200

    Remove legacy HERE autosuggest compatibility route

--- api/here-autosuggest.ts
+++
@@ -1,4 +0,0 @@
-// Compatibilité avec l'ancienne URL appelée par le formulaire.
-// Le fournisseur d'autocomplétion est désormais Geoapify ; aucune clé HERE
-// n'est utilisée par cet endpoint.
-export { default } from './geoapify-autocomplete';
