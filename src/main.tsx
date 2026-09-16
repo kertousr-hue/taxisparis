7eb325a844ecf9c90182d739801d07097ce8d44d
kertousr-hue <kertous.r@gmail.com>
2026-09-16 08:25:31 +0200

    Charge la nouvelle charte TAXINÉA sur tout le site public

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
 import './index.css';
+import './taxinea-theme.css';
 
 const container = document.getElementById('root')!;
 
